7aceb4a09266938ef3e46f3854d9175772bd42c4
JulioSergioFS <juliosergio010302@gmail.com>
2023-02-25 01:35:50 -0300

    style header and about sections

--- src/sections/AboutMe.tsx
+++ src/sections/AboutMe.tsx
@@ -1,6 +1,22 @@
+import cssIcon from "@iconify/icons-skill-icons/css";
+import figmaDark from "@iconify/icons-skill-icons/figma-dark";
+import htmlIcon from "@iconify/icons-skill-icons/html";
+import javascriptIcon from "@iconify/icons-skill-icons/javascript";
+import reactDark from "@iconify/icons-skill-icons/react-dark";
+import typescriptIcon from "@iconify/icons-skill-icons/typescript";
+import { Icon } from "@iconify/react";
 import "../styles/sections/about.scss";
 
 export function AboutMe() {
+  const logos = [
+    typescriptIcon,
+    javascriptIcon,
+    reactDark,
+    figmaDark,
+    cssIcon,
+    htmlIcon,
+  ];
+
   return (
     <div className="content about">
       <div className="image-container">
@@ -12,16 +28,29 @@ export function AboutMe() {
       </div>
       <div className="text">
         <h2 className="title">About me</h2>
-        <p>
-          I am a Junior developer with almost 2 years of formal work experience
-          seeking a new opportunity.
-        </p>
-        <p>
-          I have developed applications for various companies, I have experience
-          working in a team and dealing with problems. Currently, I am deepening
-          my knowledge in ReactJS and TypeScript.
-        </p>
-        <h4 className="read-link">Read my articles &gt;&gt;</h4>
+        <div className="text-content">
+          <p>
+            I am a Junior developer with almost 2 years of formal work
+            experience seeking a new opportunity.
+          </p>
+          <p>
+            I have developed applications for various companies, I have
+            experience working in a team and dealing with problems. Currently, I
+            am deepening my knowledge in ReactJS and TypeScript.
+          </p>
+          <a
+            className="read-link"
+            target="_blank"
+            href="https://juliosergiofs.medium.com/"
+          >
+            Read my articles &gt;&gt;
+          </a>
+        </div>
+        <div className="logos">
+          {logos.map((logo, index) => (
+            <Icon icon={logo} height={48} />
+          ))}
+        </div>
       </div>
     </div>
   );
